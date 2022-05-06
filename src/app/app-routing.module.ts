f83292746f1b3c7ea513e516f66bcaee9b108c3b
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-06 09:59:57 -0400

    Creados módulos lazy-loaded, definido layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) },
+  { path: 'bar', loadChildren: () => import('./bar/bar.module').then(m => m.BarModule) },
+  { path: '', redirectTo: '/foo', pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
